4a12784cff68d4c56ab7a2945b2263b38679866d
unknown <cintia_g24@hotmail.com>
2023-07-22 11:06:23 -0300

    version1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+
+Console.WriteLine("por favor ingrese su nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("por favor ingrese su puesto");
+var cargo = Console.ReadLine();
+Console.WriteLine("por favor ingrese su edad");
+var edad = int.Parse(Console.ReadLine());  //pasar de numero a letra//
+
+Console.WriteLine($"Hola, mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords()} años"); //ToWords pasa de numero a letra//
